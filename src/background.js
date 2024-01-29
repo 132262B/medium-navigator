017bee5e065ff9c3b3bf7d62d10bfe24f4d9f475
Igor <132262b@naver.com>
2024-01-29 21:46:27 +0900

    chore: delete annotation

--- src/background.js
+++ src/background.js
@@ -1,7 +1,1 @@
 'use strict';
-
-// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-//   if (changeInfo.status === 'complete' && tab.active) {
-//     chrome.tabs.sendMessage(tabId, {action: "init"});
-//   }
-// });
