d3541e29449a82d6690e07d6cbe32258c50878eb
Igor <132262b@naver.com>
2025-09-22 23:13:31 +0900

    style: We have revised the overall design of the navigation to resemble Medium.

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -19,7 +19,7 @@ export const icons = {
             <path d="m22 22-5-10-5 10"></path>
             <path d="M14 18h6"></path>
           </svg>`,
-  resetTranslation: `<svg fill="#ffffff" width="12" height="12" viewBox="0 0 512 512" data-name="Layer 1" id="Layer_1" xmlns="http://www.w3.org/2000/svg">
+  resetTranslation: `<svg fill="#333" width="12" height="12" viewBox="0 0 512 512" data-name="Layer 1" id="Layer_1" xmlns="http://www.w3.org/2000/svg">
             <path d="M64,256H34A222,222,0,0,1,430,118.15V85h30V190H355V160h67.27A192.21,192.21,0,0,0,256,64C150.13,64,64,150.13,64,256Zm384,0c0,105.87-86.13,192-192,192A192.21,192.21,0,0,1,89.73,352H157V322H52V427H82V393.85A222,222,0,0,0,478,256Z"/>
           </svg>
   `,
